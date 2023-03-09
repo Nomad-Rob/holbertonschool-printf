e43a0faa61a29a80c531af823e70e65842670bd1
Nomad-Rob <rfarley89@gmail.com>
2023-03-09 12:42:22 -0800

    Created the get_function file that determines what specifier to use

--- main.h
+++ main.h
@@ -1,10 +1,33 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stddef.h>
 
-int _printf(const char *format, ..);
+/**
+* struct specifiers - structure to find the correct function
+* and specifiers
+* @letter: specifier conversion
+* @handler: function
+*
+*/
+
 typedef struct specifiers
 {
 	char letter;
-	void (*handler)(va_list);
-};
+	void (*handle)(va_list);
+}
+
+//Main functions//
+int _printf(const char *format, ...);
+int _putchar(char c)
+void handle_char(va_list args)
+void handle_string(va_list args)
+void handle_percent(va_list args)
+
+
+
+
 #endif
