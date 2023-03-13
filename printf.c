0e2849e6dafac419bbe14cdfd755241a30baf4ab
Dom <you@example.com>
2023-03-13 15:53:39 -0700

    condenced printf.c

--- printf.c
+++ printf.c
@@ -4,55 +4,33 @@
 * _printf - prints a formatted string to output
 * @format: input string to format with specifiers to print
 * Return: number of characters printed
-*
-*
-*
-*
-*
 */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
-	int (*function)(va_list) = NULL;
+        int count = 0;
+        va_list args;
+        int (*function)(va_list);
 
-	va_start(args, format);
+        va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%' && *(format + 1) != '%')
-		{
-			format++;
-			function = get_function(format);
-			if (*(format) == '\0')
-				return (-1);
+        while (*format)
+        {
+                if (*format == '%' && *(format + 1) != '%')
+                {
+                        format++, function = get_function(format);
+                        if (!function)
+                                _putchar(*(format - 1)), _putchar(*format), count += 2;
+                        else
+                                count += function(args);
+                }
+                else if (*format == '%' && *(format + 1) == '%')
+                        format++, _putchar('%'), count++;
+                else
+                        _putchar(*format), count++;
 
-			else if (function == NULL)
-			{
-				_putchar(*(format - 1));
-				_putchar(*format);
-				count += 2;
-			}
-			else
+                format++;
+        }
 
-				count += function(args);
-
-		}
-		else if (*format == '%' && *(format + 1) == '%')
-		{
-			format++;
-			_putchar('%');
-			count++;
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-
-			format++;
-	}
-	va_end(args);
-	return (count);
+        va_end(args);
+        return (count);
 }
