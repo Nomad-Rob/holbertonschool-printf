59243b2872628e0f8294807282d6c0a1157e5fd6
Nomad-Rob <rfarley89@gmail.com>
2023-03-13 13:07:37 -0700

    adding print_help file to assist with string count and int specifiers

---
+++ print_help.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+  * print_number - printing input number to base 10
+  * @n: number to be based 10
+  *
+  * Return: void
+  */
+
+int  print_number(unsigned int n)
+{
+	int retval;
+	unsigned int num = n;
+
+	if (num / 10)
+		print_number(num / 10);
+	retval = _putchar('0' + num % 10);
+	if (retval == -1)
+	{
+		return (retval);
+	}
+	return (retval);
+}
+
+/**
+* countDigits - count how many digits the number is
+* @num: 
+*
+* Return: the count
+*/
+int countDigits(unsigned int num)
+{
+	int count = 0;
+
+	while (num > 0)
+	{
+		count++;
+		num /= 10;
+	}
+	return (count);
+}
+
+/**
+ * _strlen - count the number of characters in a string
+ * @str: pointer to a string
+ *
+ * Return: number of characters in the string
+ */
+
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (*(str + count))
+		count++;
+
+	return (count);
+}
