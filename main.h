2ac7f5df86b9772dc9036cd0fe2c95ac9f8f21f4
Nomad-Rob <rfarley89@gmail.com>
2023-03-13 13:42:58 -0700

    fixed main head issue in main.h

--- main.h
+++ main.h
@@ -30,12 +30,9 @@ int handle_percent(__attribute__((unused))va_list args);
 int (*get_function(const char *specifier))(va_list);
 int handle_decimal(va_list args);
 int handle_integer(va_list args);
-<<<<<<< HEAD
 int countBinary(unsigned int num)
-
-=======
 int print_number(unsigned int n);
 int countDigits(unsigned int num);
 int _strlen(char *str);
->>>>>>> dd4b1bf1c45f4234ec4130a851cfb12540705183
+
 #endif
